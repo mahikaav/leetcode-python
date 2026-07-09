cad47c57d119e828f3e8a1088fb90b667e1a3af6
Mahika Annie Verghese <145218498+mahikaav@users.noreply.github.com>
2026-07-09 16:21:24 -0400

    Time: 0 ms (100%), Space: 19.3 MB (55.13%) - LeetHub

--- LeetCode/Easy/2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
+++ LeetCode/Easy/2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
@@ -2,8 +2,8 @@ class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
         x = 0
         for var in operations:
-            if (var == "--X" or var =="X--"):
+            if var[1] == "+":
+                x= x+1
+            else:
                 x=x-1
-            elif (var == "X++" or var == "++X"):
-                x=x+1
         return x
