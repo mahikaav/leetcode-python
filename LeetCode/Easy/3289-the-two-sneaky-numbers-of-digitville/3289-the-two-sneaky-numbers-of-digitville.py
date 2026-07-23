78212b1520d756e2c27dc573be85f42cd879bf7f
Mahika Annie Verghese <145218498+mahikaav@users.noreply.github.com>
2026-07-23 15:38:44 -0400

    Time: 0 ms (100%), Space: 19.3 MB (60.87%) - LeetHub

--- LeetCode/Easy/3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.py
+++ LeetCode/Easy/3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.py
@@ -1,12 +1,9 @@
 class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        ctr = 0
         ans = []
         nums.sort()
-        for i in range(0,n):
-            if (i - ctr - nums[i]) != 0:
-                ctr += 1
-                ans.append(nums[i])
+        for i in range(n-1):
+            if (nums[i+1] - nums[i]) == 0: ans.append(nums[i])
         
         return ans
